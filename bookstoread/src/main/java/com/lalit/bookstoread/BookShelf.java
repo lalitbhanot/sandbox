8c675356e7388da70cc166b23b9461dae03bb5d8
lalit bhanot <lalitthebest@gmail.com>
2025-06-24 17:34:57 +0530

    1st Feature: As a user, I want to add multiple books to my bookshelf so that I can read them later.

--- bookstoread/src/main/java/com/lalit/bookstoread/BookShelf.java
+++ bookstoread/src/main/java/com/lalit/bookstoread/BookShelf.java
@@ -1,17 +1,24 @@
 package com.lalit.bookstoread;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
 public class BookShelf {
     private List<String> books = new ArrayList<>();
 
     public List<String> books() {
-        return books ;
+        // Returing Immutable class
+        return Collections.unmodifiableList(books);
+
     }
-    public void  addBooks(String bookname)
-    {
-            books.add(bookname) ;
+
+    public void add(String... booksToAdd) {   //For each book executes the lambda expression book -> books.add(book), which adds the current book to the books collection.
+        //   Arrays.stream(booksToAdd).forEach(book -> books.add(book));
+        // using method referece
+        Arrays.stream(booksToAdd).forEach(books::add);
+
     }
 
     @Override
--- bookstoread/src/test/java/com/lalit/bookstoread/BookShelfTest.java
+++ bookstoread/src/test/java/com/lalit/bookstoread/BookShelfTest.java
@@ -4,54 +4,79 @@ import org.junit.jupiter.api.*;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("A bookshelf")
-public class BookShelfTest
-{
+public class BookShelfTest {
+   private  BookShelf bookShelf ;
     @BeforeAll
     @DisplayName("Before All Method")
     static void BeforeAllTestShelf() {
-        System.out.println("Before All runs first") ;
+        System.out.println("Before All runs first");
     }
 
     @BeforeEach
-    void BeforeEachTestShelf() {
-        System.out.println("Runs Before  Each Test Begins ") ;
+    void init() {
+        bookShelf= new BookShelf();
+        System.out.println("Runs Before  Each Test Begins ");
     }
 
     @Test
     @DisplayName("is empty when no book is added to it")
     public void shelfEmptyWhenNoBookAdded() throws Exception {
-        BookShelf bookShelf = new BookShelf();
+
         List<String> books = bookShelf.books();
-        assertTrue(books.isEmpty(),()->"Book Shelf should be empty") ;
+        assertTrue(books.isEmpty(), () -> "Book Shelf should be empty");
         // see junit_learning.md point 1
-}
-@Test
-@DisplayName("Shelf To String Should Print Book Count And Titles")
+    }
 
-public void shelfToStringShouldPrintBookCountAndTitles() throws Exception
-{
-    BookShelf shelf = new BookShelf();
-    List<String> books = shelf.books();
-    shelf.addBooks("The Phoenix Project");
-    shelf.addBooks("Java 8 in Action");
-    String shelfStr = shelf.toString();
-    assertAll(()  -> assertTrue(shelfStr.contains("The Phoenix Project"),  "1st book title "),
-            () -> assertTrue(shelfStr.contains("Java 8 in Action") , "2nd book title  "),
-            () -> assertTrue(shelf.books().size()==2 , "Book  count "));
+    @Test
+    @DisplayName("Shelf To String Should Print Book Count And Titles")
 
-}
+    public void shelfToStringShouldPrintBookCountAndTitles() throws Exception {
+
+        List<String> books = bookShelf.books();
+        bookShelf.add("The Phoenix Project", "Java 8 in Action", "New Book added");
+        //  shelf.addBooks();
+        String shelfStr = bookShelf.toString();
+        assertAll(() -> assertTrue(shelfStr.contains("The Phoenix Project"), () -> "1st book title "),
+                () -> assertTrue(shelfStr.contains("Java 8 in Action"), () -> "2nd book title  "),
+                () -> assertTrue(bookShelf.books().size() > 0, () -> " Current Book  count " + bookShelf.books().size()));
+
+    }
+
+    @Test
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+
+        bookShelf.add();
+        List<String> books = bookShelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
+
+    @Test
+    void booksReturnedFromBookShelfIsImmutableForClient() {
+
+        bookShelf.add("Effective Java", "Code Complete");
+        List<String> books = bookShelf.books(); // Retreiveing the books
+        try {
+            books.add("The Mythical Man-Month"); // adding the books to book list basically modifing the booklist
+                                                //the collection that we received from the books method
+            // is mutable so we were able to successfully add a book to it.
+            fail(() -> "Should not be able to add book to books");
+        } catch (Exception e) {
+            assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException.");
+        }
+    }
+
+
     @AfterEach
     void afterEachTestShelf() {
-        System.out.println("Runs After  Each  Test Completes ") ;
+        System.out.println("Runs After  Each  Test Completes ");
     }
 
     @AfterAll
     static void AfterAllTestShelf() {
-        System.out.println("Last method to run for clean up ") ;
+        System.out.println("Last method to run for clean up ");
 
     }
 
